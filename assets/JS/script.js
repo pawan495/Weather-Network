9bfb964a94b5e5e4864e815d0fa53e05696ddc00
pawan495 <pawanpreetkaur495@gmail.com>
2022-03-21 14:10:12 -0400

    second commit

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,8 +3,21 @@
 $(".btn").on("click", function (e){
     e.preventDefault();
     var city =$("#search-input").val();
-}
-)
+    cityOnload = localStorage.setItem("cityStored", city);
+    if(city ===""){
+        return
+    }else{
+        getWeatherDetails(city, true);
+    }
+});
+
+$("#saved-cities").on("click", function (e) {
+    if (!e.target.matches(".btnCity")) return;
+
+    var saveCity = e.target.id;
+    getWeatherDetails(saveCity, false);
+    console.log(saveCity);
+});
 
 
 // calling function to render the last searched city
